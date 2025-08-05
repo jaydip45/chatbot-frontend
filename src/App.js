ab403a76f726647a00769d90a082fb56ff40202f
jaydip45 <gadhavijaydip45@gmail.com>
2025-08-05 22:14:27 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Frontend on localhost:3000
+    </div>
+  );
+}
+
+export default App;
