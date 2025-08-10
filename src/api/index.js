ed624ca7d0e814bf2a0685814d6bc8718017f6c6
jaydip45 <gadhavijaydip45@gmail.com>
2025-08-10 22:51:19 +0530

    create env file

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL;
 
 export const createConversation = (data) =>
   axios.post(`${API_BASE}/conversation/add`, data);
